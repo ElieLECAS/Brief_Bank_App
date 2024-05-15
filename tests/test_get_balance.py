f67a6aa073771679213085b907f68611bfa59367
Elie LECAS <elielecas00@gmail.com>
2024-05-15 16:30:15 +0200

    commence get balance

--- tests/test_get_balance.py
+++ tests/test_get_balance.py
@@ -2,4 +2,12 @@ import pytest
 import tuto.models as models
 
 
+# Test_get_balance_initial
 
+class TestGetBalance:
+    def setup_method(self):
+        self.account = models.Account()
+
+
+    def test_get_balance_initial(self):
+        pass
